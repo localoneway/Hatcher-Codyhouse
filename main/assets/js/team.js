51e65a725822bc0bd32974190e537f00e8b116da
localoneway <andrewdelman@gmail.com>
2021-10-18 10:33:44 -0700

    team page contact

--- main/assets/js/team.js
+++ main/assets/js/team.js
@@ -1,3 +1,17 @@
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('pizzaOrder');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
+
 var PageTransition = document.getElementsByClassName('text-anim');
 
 new PageTransition({
