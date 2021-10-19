a4d957024da1e1aa2c13ef98b839c3903e6a0448
localoneway <andrewdelman@gmail.com>
2021-10-19 09:00:05 -0700

    initiate the dom

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -3292,7 +3292,29 @@ function initContactMap(wrapper) {
       animateLoader(3000, loaderScaleDown, 0.9);
     }
   });
-
+  new PageTransition({
+    afterEnter: function(newContent, link) {
+      // slideshow
+      var slideshowEl = newContent.getElementsByClassName('slideshow');
+      if(slideshowEl.length > 0) {
+        new Slideshow({
+          element: slideshowEl[0],
+          navigation: true, // show dots navigation
+          autoplay : false, // enable/disable autoplay
+          autoplayInterval : false, // in milliseconds - default is 5000 (5s)
+          autoplayOnHover: false, // do not pause autoplay on hover
+          swipe : false // enable/disable swipe
+        }); 
+      }
+  
+      // animated headline
+      var headline = newContent.getElementsByClassName('text-anim');
+      if(headline.length > 0) {
+        new TextAnim(headline[0]);
+      }
+    },
+    // additional options here
+  });
   function animateLoader(duration, startValue, finalValue, cb) {
     // takes care of animating the loader element
     var currentTime = false;
